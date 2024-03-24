26468669f89ab25cde8296d70038baf2864fc510
Willian Silva Ferreira <112612301+NailliwFerreir@users.noreply.github.com>
2024-03-23 23:04:45 -0300

    chore: remove typescript doc link

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,6 @@
 // import { onRequest } from "firebase-functions/v2/https";
 // import * as logger from "firebase-functions/logger";
 
-// https://firebase.google.com/docs/functions/typescript
-
 // export const helloWorld = onRequest((request, response) => {
 //   logger.info("Hello logs!", { structuredData: true });
 //   response.send("Hello from Firebase!");
